d422e404c78372d35360bc40b8936aca966db4a0
Rafael Fernandes Marques <rafael.marques@radixeng.com.br>
2022-08-01 20:30:11 -0300

    Adiciona scripts de recuperação para ontologia GO

---
+++ UpdateGo.ps1
@@ -0,0 +1,8 @@
+﻿$source = 'http://purl.obolibrary.org/obo/go.obo'
+$destination = './data/go.obo'
+
+if (Test-Path $destination) {
+  Remove-Item $destination
+}
+
+Invoke-WebRequest -Uri $source -OutFile $destination
